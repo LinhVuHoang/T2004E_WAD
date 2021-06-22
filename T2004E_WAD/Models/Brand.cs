13518a1108d1e10756d6838e551253dd56639934
LinhVuHoang <56089011+LinhVuHoang@users.noreply.github.com>
2021-06-22 16:46:04 +0700

    Update code

---
+++ T2004E_WAD/Models/Brand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace T2004E_WAD.Models
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public string NameB { get; set; }
+
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+        
+    }
+}
